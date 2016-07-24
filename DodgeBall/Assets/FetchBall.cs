5289243bf87b4760d00d9138fd67cec173a76c6d
Mike Shultz <shultzm@gmail.com>
2016-07-23 21:35:20 -0600

    Bunch of basics, including ball fetching.

---
+++ DodgeBall/Assets/FetchBall.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class FetchBall : MonoBehaviour
+{
+
+    [Tooltip("The amount of force to apply to the ball")]
+    public float pullForce;
+
+    private void Start()
+    {
+        if (GetComponent<VRTK_SimplePointer>() == null)
+        {
+            Debug.LogError("VRTK_ControllerPointerEvents_ListenerExample is required to be attached to a SteamVR Controller that has the VRTK_SimplePointer script attached to it");
+            return;
+        }
+
+        //Setup controller event listeners
+        GetComponent<VRTK_SimplePointer>().DestinationMarkerEnter += new DestinationMarkerEventHandler(DoPointerIn);
+        GetComponent<VRTK_SimplePointer>().DestinationMarkerExit += new DestinationMarkerEventHandler(DoPointerOut);
+    }
+
+    private void DoPointerIn(object sender, DestinationMarkerEventArgs e)
+    {
+        if (e.target.tag == "Ball")
+        {
+            Debug.Log("Fetching Ball");
+            Vector3 direction = transform.position - e.target.transform.position;
+            //Quaternion towardPlayer = Quaternion.LookRotation();
+            e.target.GetComponent<Rigidbody>().AddForce(direction.normalized * pullForce * Time.deltaTime);
+        }
+    }
+
+    private void DoPointerOut(object sender, DestinationMarkerEventArgs e)
+    {
+        if (e.target.tag == "Ball")
+        {
+            Debug.Log("Releaseing Ball");
+        }
+    }
+}
